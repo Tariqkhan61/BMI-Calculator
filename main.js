14ca25bcca0c8dae5eb5e388679f807b02db75b2
Tariqkhan61 <161859910+Tariqkhan61@users.noreply.github.com>
2024-05-28 12:29:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// We are going to create BMI Calculator (Body-Mass-Index).
+import inquirer from "inquirer";
+const Questions = [
+    { type: 'number', name: 'weight', message: 'weight:(kg):' },
+    { type: 'number', name: 'height', message: 'height:(m):' },
+];
+const calculateBMI = (weight, height) => weight / (height * height);
+const main = async () => {
+    const { weight, height } = await inquirer.prompt(Questions);
+    console.log(`BMI:${calculateBMI(weight, height).toFixed(2)}`);
+};
+main();
